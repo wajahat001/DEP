3fb8be1b12223317182e6177e4b913125481ed6b
Muhammad Wajahat <wajahatchaudy1640@gmail.com>
2024-08-13 16:14:58 +0500

    Task2

--- Task2/script.js
+++ Task2/script.js
@@ -5,20 +5,56 @@ function main(){
         const body1 = document.querySelector(".body1");
         const arrow = document.querySelector(".up");
         
-        // Check the current position and toggle it
+        
         if (body1.style.left === "20px") {
             body1.style.left = "-1200px";
-            arrow.setAttribute("src", "arrow-down.svg"); // Change the arrow direction
+            arrow.setAttribute("src", "arrow-down.svg"); 
         } else {
             body1.style.left = "20px";
-            arrow.setAttribute("src", "arrow-up.svg"); // Change the arrow direction
+            arrow.setAttribute("src", "arrow-up.svg"); 
+        }
+    });
+    document.querySelector(".head2").addEventListener("click", () => {
+        const body2 = document.querySelector(".body2");
+        const arrow = document.querySelector(".up");
+        
+        
+        if (body2.style.left === "20px") {
+            body2.style.left = "-1200px";
+            arrow.setAttribute("src", "arrow-down.svg"); 
+        } else {
+            body2.style.left = "20px";
+            arrow.setAttribute("src", "arrow-up.svg"); 
         }
     });
 
+
     
-    let box=document.querySelector(".box")
-    alert(box.innerHTML)
-    alert.style.margin="200px"
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("openModal");
+
+let span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change",(e)=>{
+     let output=document.querySelector(".output")
+     output.innerHTML=((parseInt(e.target.value)/100)*100)
+     
+    
+   
+})
+
 
 }
 main()
