e2e679b787a6e4454740b22c3abe25036fd0ff45
Muhammad Wajahat <wajahatchaudy1640@gmail.com>
2024-08-16 14:56:44 +0500

    Task2

--- Task2/script.js
+++ Task2/script.js
@@ -3,28 +3,28 @@ function main(){
 
     document.querySelector(".head1").addEventListener("click", () => {
         const body1 = document.querySelector(".body1");
-        const arrow = document.querySelector(".up");
+     
         
         
         if (body1.style.left === "20px") {
             body1.style.left = "-1200px";
-            arrow.setAttribute("src", "arrow-down.svg"); 
+            
         } else {
             body1.style.left = "20px";
-            arrow.setAttribute("src", "arrow-up.svg"); 
+            
         }
     });
     document.querySelector(".head2").addEventListener("click", () => {
         const body2 = document.querySelector(".body2");
-        const arrow = document.querySelector(".up");
+        
         
         
         if (body2.style.left === "20px") {
             body2.style.left = "-1200px";
-            arrow.setAttribute("src", "arrow-down.svg"); 
+           
         } else {
             body2.style.left = "20px";
-            arrow.setAttribute("src", "arrow-up.svg"); 
+            
         }
     });
 
